d92c1b64b0c4956f9485886e93e91fb5f79d3b3c
Baptiste Bourut <bbourut@iutbayonne.univ-pau.fr>
2022-11-17 11:52:09 +0100

    observateur cpp

--- TP3/Observateur.cpp
+++ TP3/Observateur.cpp
@@ -5,3 +5,19 @@ void Observateur::Update(const Observable* observable)const {
 	cout << "Change detected in " << observable->GetName() << ", value : "
 		<< observable->Statut() << endl;
 }
+void Observateur::AddObs(Observable* obs) {
+	m_list.push_back(obs);
+}
+
+void Observateur::DelObs(Observable* obs) {
+	iterator it = find(m_list.begin(), m_list.end(), obs);
+	if (it != m_list.end()) {
+		m_list.erase(it);
+	}
+}
+Observateur::~Observateur() {
+	iterator ite = m_list.end();
+	for (iterator itb = m_list.begin(); itb != ite; itb++) {
+		(*itb)->DelObs(this);
+	}
+}
